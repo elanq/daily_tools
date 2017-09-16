9c3eb58ebed2dde950f70a03a9841509beec6635
Elan Qisthi <elan.qisthi@bukalapak.com>
2017-09-16 09:44:37 +0700

    build errors in handler

--- banker/http/handler.go
+++ banker/http/handler.go
@@ -16,6 +16,18 @@ import (
 	"github.com/elanq/daily_tools/banker/utility"
 )
 
+const (
+	ErrInternalError         = errors.New("error while processing request")
+	ErrContentNotFound       = errors.New("content not found")
+	ErrTransactionExists     = errors.New("Transaction already exists")
+	ErrYearNotExists         = errors.New("should specify year as param")
+	ErrYearAndMonthNotExists = errors.New("should specify month and year as param")
+	ErrInvalidForm           = errors.New("Invalid form key")
+	ErrInvalidFile           = errors.New("Cannot read specified file")
+	ErrInvalidCSV            = errors.New("Invalid csv format")
+	ErrorSavingToDb          = errors.New("Error while saving file to db")
+)
+
 //Handler type. used to reference csv reader, csv key, year key and mongodriver
 type Handler struct {
 	Reader      *parser.BankReader
@@ -37,7 +49,7 @@ func NewHandler(reader *parser.BankReader, driver *db.MongoDriver) *Handler {
 //Insert content to db
 func (h *Handler) saveContent(bankContents []*model.BankContent) error {
 	if h.checkContent(bankContents[0], bankContents[len(bankContents)-1]) {
-		return errors.New("Transactions already exists")
+		return ErrTransactionExists
 	}
 
 	for _, content := range bankContents {
@@ -68,8 +80,7 @@ func (h *Handler) YearlyReport(w http.ResponseWriter, r *http.Request) {
 	var results []model.BankContent
 
 	if year == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("should specify year as param"))
+		http.Error(w, ErrYearNotExists.Error(), http.StatusBadRequest)
 		return
 	}
 	minTime := parser.ParseDate("01/01/" + year)
@@ -78,14 +89,12 @@ func (h *Handler) YearlyReport(w http.ResponseWriter, r *http.Request) {
 	err := h.fetchContent(minTime, maxTime, &results)
 
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("error while processing request"))
+		http.Error(w, ErrInternalError.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	if len(results) < 1 {
-		w.WriteHeader(http.StatusNotFound)
-		w.Write(([]byte("404 - Content not found")))
+		http.Error(w, ErrContentNotFound.Error(), http.StatusNotFound)
 		return
 	}
 
@@ -106,8 +115,7 @@ func (h *Handler) MonthlyReport(w http.ResponseWriter, r *http.Request) {
 	var results []model.BankContent
 
 	if month == "" || year == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("should specify month and year as param"))
+		http.Error(w, ErrYearAndMonthNotExists.Error(), http.StatusBadRequest)
 		return
 	}
 
@@ -116,14 +124,12 @@ func (h *Handler) MonthlyReport(w http.ResponseWriter, r *http.Request) {
 
 	err := h.fetchContent(minTime, maxTime, &results)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("error while processing request"))
+		http.Error(w, ErrInternalError.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	if len(results) < 1 {
-		w.WriteHeader(http.StatusNotFound)
-		w.Write(([]byte("404 - Content not found")))
+		http.Error(w, ErrContentNotFound.Error(), http.StatusNotFound)
 		return
 	}
 
@@ -150,17 +156,14 @@ func returnSummary(w *http.ResponseWriter, r *http.Request, results *[]model.Ban
 func (h *Handler) FileUpload(w http.ResponseWriter, r *http.Request) {
 	file, _, err := r.FormFile(h.CSVKey)
 	if err != nil {
-		w.WriteHeader(400)
-		w.Write([]byte("400 - Invalid form key"))
+		http.Error(w, ErrInvalidForm.Error(), http.StatusBadRequest)
 		return
 	}
 
 	defer file.Close()
-
 	rawBytes, err := ioutil.ReadAll(file)
 	if err != nil {
-		w.WriteHeader(400)
-		w.Write([]byte("400 - Cannot read specified file"))
+		http.Error(w, ErrInvalidFile.Error(), http.StatusBadRequest)
 		return
 	}
 
@@ -172,15 +175,13 @@ func (h *Handler) FileUpload(w http.ResponseWriter, r *http.Request) {
 	h.Reader.ReadBytes(rawBytes)
 	bankContents, err := h.Reader.ParseContent(year)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("400 - Invalid csv format"))
+		http.Error(w, ErrInvalidCSV.Error(), http.StatusBadRequest)
 		return
 	}
 
 	err = h.saveContent(bankContents)
 	if err != nil {
-		w.WriteHeader(http.StatusNotModified)
-		w.Write([]byte("304 - Error while saving file to db"))
+		http.Error(w, ErrorSavingToDb.Error(), http.StatusNotModified)
 		return
 	}
 
